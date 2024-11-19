7b4b4b1ed58672cdac4360eee571399b2f438b36
Mukesh Singh Rana <mukesh.rana@statnett.no>
2024-11-19 10:11:21 +0100

    Initial commit for LandlordPro

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+i
+9src/main/java/com/landlordpro/LandlordProApplication.java,b/f/bff0633b7d76619df74649ce1ed770f241a21b41
---
+++ src/main/java/com/landlordpro/LandlordProApplication.java
@@ -0,0 +1,12 @@
+package com.landlordpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordProApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LandlordProApplication.class, args);
+    }
+}
+
