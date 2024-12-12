415886243d566aefafeb4a7fc4ae982a5ec6aab4
Mukesh Singh Rana <mukesh.rana@statnett.no>
2024-12-12 12:13:37 +0100

    delete expense

--- src/main/java/com/landlordpro/model/Expense.java
+++ src/main/java/com/landlordpro/model/Expense.java
@@ -2,6 +2,7 @@ package com.landlordpro.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,15 +10,17 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL) // Avoid serializing null values
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Expense {
 
     @JsonProperty("id")
-    private String id;
+    private String id = UUID.randomUUID().toString(); // Auto-generate a unique id using UUID
 
     @JsonProperty("name")
     private String name;
