c3751aa0cce357a0dda0b6bb879df1db67f50fc4
Mukesh Singh Rana <mukesh.rana@statnett.no>
2024-12-17 18:40:05 +0100

    tenant

--- src/main/java/com/landlordpro/controller/TenantController.java
+++ src/main/java/com/landlordpro/controller/TenantController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.landlordpro.service.ApartmentService;
+
 @Controller
 public class TenantController {
 
+    private final ApartmentService apartmentService;
+
+    public TenantController(ApartmentService apartmentService) {
+        this.apartmentService = apartmentService;
+    }
+
     @GetMapping("/registerTenant")
     public String registerIncome(Model model) {
+        model.addAttribute("apartmentNames", apartmentService.apartmentNames());
         model.addAttribute("page", "registerTenant");
         return "registerTenant";
     }
