7d1f5d976e651e211b970b94336f819a3787c2f2
Mukesh Singh Rana <mukesh.rana@statnett.no>
2024-12-20 21:28:46 +0100

    fix: primery key for contacts

--- src/main/resources/db/migration/V4__Create_Contact_Table.sql
+++ src/main/resources/db/migration/V4__Create_Contact_Table.sql
@@ -1,7 +1,7 @@
 -- Create the `contact` table
 CREATE TABLE contact
 (
-    id           VARCHAR(36)  NOT NULL PRIMARY KEY, -- UUID with a length of 36 characters
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY, -- Primary key for the user
     name         VARCHAR(255) NOT NULL,             -- Name of the user, required
     email        VARCHAR(255) NOT NULL,             -- Email of the user, required
     message      TEXT         NOT NULL,             -- Message content, required
