7bca164f8eb952a8459c283c47c21d56a879316e
Mukesh Singh Rana <mukesh.rana@statnett.no>
2024-12-23 18:10:12 +0100

    fix: username in view

--- src/main/java/com/landlordpro/config/GlobalControllerAdvice.java
+++ src/main/java/com/landlordpro/config/GlobalControllerAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.landlordpro.security.CustomUserDetails;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
@@ -13,6 +15,11 @@ public class GlobalControllerAdvice {
     public void addAuthStatus(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         boolean isAuthenticated = authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getPrincipal());
+        if (isAuthenticated) {
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            String username = userDetails.getName();
+            model.addAttribute("username", username);
+        }
         model.addAttribute("isAuthenticated", isAuthenticated);
     }
 }
--- src/main/java/com/landlordpro/controller/HomeController.java
+++ src/main/java/com/landlordpro/controller/HomeController.java
@@ -5,20 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.landlordpro.security.CustomUserDetails;
-
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String homePage(Model model, Authentication authentication) {
-        // Get the logged-in user's details
-        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        String username = userDetails.getName();  // Or you can use userDetails.getId() to fetch the user ID
-
-        // Add the user's name to the model
-        model.addAttribute("username", username);
-
         return "home"; // Return the Thymeleaf view (home.html)
     }
 
