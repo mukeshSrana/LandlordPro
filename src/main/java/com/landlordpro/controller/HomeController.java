9426586a9a141e61149d0cf816d56ba93e94b150
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-02 20:41:33 +0100

    fix: security at html

--- src/main/java/com/landlordpro/controller/HomeController.java
+++ src/main/java/com/landlordpro/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.landlordpro.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String homePage(Model model, Authentication authentication) {
+    public String homePage() {
         return "home";
     }
 
