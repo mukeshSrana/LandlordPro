26e63da62c2933f504fd114e4accbcf18ccede5c
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-03 15:30:35 +0100

    fix: user

--- src/main/java/com/landlordpro/controller/HomeController.java
+++ src/main/java/com/landlordpro/controller/HomeController.java
@@ -1,12 +1,8 @@
 package com.landlordpro.controller;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.landlordpro.dto.enums.UserRole;
 
