4f8bda989b4ec388e37e299f5c9f39350f03343c
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-05 12:07:34 +0100

    fix: contact

--- src/main/java/com/landlordpro/dto/ContactDto.java
+++ src/main/java/com/landlordpro/dto/ContactDto.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,14 +19,16 @@ public class ContactDto {
     private UUID id;
 
     @NotBlank(message = "Reference is required")
+    @Size(min = 10, max = 10, message = "Reference must be of 10 characters")
     private String reference;
 
     @NotBlank(message = "Name is required")
-    @Size(min = 2, max = 50, message = "Name must be between 2 and 100 characters")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Please enter a valid email address")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Please enter a valid email address (e.g., user@example.com)")
     private String email;
 
     @NotBlank(message = "Message is required")
