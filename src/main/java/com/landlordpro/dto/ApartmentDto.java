23abff60dffa1fe75b56378f8c1aae9d1845f011
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-06 09:31:27 +0100

    fix: contact

--- src/main/java/com/landlordpro/dto/ApartmentDto.java
+++ src/main/java/com/landlordpro/dto/ApartmentDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,38 +15,71 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ApartmentDto {
 
+    public static final String NAME_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøå\s]+$";
+    public static final String ADDRESS_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøå0-9\s.,#-]+$";
+    public static final String PINCODE_PATTERN_FOUR_DIGITS = "^[0-9]{4}$";
+    public static final String APARTMENT_NAME_PATTERN_LETTER_WITHOUT_SPACES = "^[a-zA-Z0-9]+$";
+
     private UUID id;  // UUID for apartment
 
-    @NotBlank(message = "Apartment short name is mandatory")  // Ensure it's not blank
-    @Size(max = 255, message = "Apartment short name should be up to 255 characters")  // Maximum length of 255
+    @NotBlank(message = "Apartment short name is mandatory")
+    @Size(min = 2, max = 40, message = "Apartment short name must be between 2 and 40 characters")
+    @Pattern(
+        regexp = APARTMENT_NAME_PATTERN_LETTER_WITHOUT_SPACES,
+        message = "Name must contain only letters without spaces or special characters"
+    )
     private String apartmentShortName;
 
-    @NotBlank(message = "Owner name is mandatory")  // Ensure it's not blank
-    @Size(max = 100, message = "Owner name should be up to 100 characters")  // Maximum length of 100
+    @NotBlank(message = "Owner name is mandatory")
+    @Size(min = 2, max = 100, message = "Owner name must be between 2 and 100 characters")
+    @Pattern(
+        regexp = NAME_PATTERN_LETTER_AND_SPACES,
+        message = "Owner name must contain only letters and spaces"
+    )
     private String ownerName;
 
-    @NotBlank(message = "Address line 1 is mandatory")  // Ensure it's not blank
-    @Size(max = 255, message = "Address line 1 should be up to 255 characters")  // Maximum length of 255
+    @NotBlank(message = "AddressLine1 is mandatory")
+    @Size(min = 2, max = 100, message = "AddressLine1 must be between 2 and 100 characters")
+    @Pattern(
+        regexp = ADDRESS_PATTERN_LETTER_AND_SPACES,
+        message = "AddressLine1 must contain only letters, spaces or special characters"
+    )
     private String addressLine1;
 
-    @Size(max = 255, message = "Address line 2 should be up to 255 characters")  // Optional but max length of 255
+    @Size( max = 100, message = "AddressLine2 must be between 0 and 100 characters")
+    @Pattern(
+        regexp = ADDRESS_PATTERN_LETTER_AND_SPACES,
+        message = "AddressLine2 must contain only letters, spaces or special characters"
+    )
     private String addressLine2;
 
-    @NotBlank(message = "Pincode is mandatory")  // Ensure it's not blank
-    @Size(max = 10, message = "Pincode should be up to 10 characters")  // Maximum length of 10
+    @NotBlank(message = "Pincode is mandatory")
+    @Size(min = 4, max = 4, message = "Pincode must be of 4 digits")
+    @Pattern(
+        regexp = PINCODE_PATTERN_FOUR_DIGITS,
+        message = "Pincode must contain only 4 digits"
+    )
     private String pincode;
 
-    @NotBlank(message = "City is mandatory")  // Ensure it's not blank
-    @Size(max = 100, message = "City should be up to 100 characters")  // Maximum length of 100
+    @NotBlank(message = "City is mandatory")
+    @Size(max = 100, message = "City should be up to 100 characters")
+    @Pattern(
+        regexp = NAME_PATTERN_LETTER_AND_SPACES,
+        message = "City must contain only letters and spaces"
+    )
     private String city;
 
-    @NotBlank(message = "Country is mandatory")  // Ensure it's not blank
-    @Size(max = 100, message = "Country should be up to 100 characters")  // Maximum length of 100
+    @NotBlank(message = "Country is mandatory")
+    @Size(max = 100, message = "Country should be up to 100 characters")
+    @Pattern(
+        regexp = NAME_PATTERN_LETTER_AND_SPACES,
+        message = "Country must contain only letters and spaces"
+    )
     private String country;
 
-    private LocalDateTime createdDate;  // Date apartment was created
+    private LocalDateTime createdDate;
 
-    private LocalDateTime updatedDate;  // Last updated date
+    private LocalDateTime updatedDate;
 
-    private UUID userId;  // Foreign key to user (user_id)
+    private UUID userId;
 }
