3ca2fbe9697b829426e55e9fbe9b2bb43c0dae5e
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-06 10:25:31 +0100

    fix: refactoring

--- src/main/java/com/landlordpro/dto/constants/Patterns.java
+++ src/main/java/com/landlordpro/dto/constants/Patterns.java
@@ -1,7 +1,7 @@
 package com.landlordpro.dto.constants;
 
 public class Patterns {
-    public static final String NAME_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé\\s]+$";
+    public static final String NAME_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé\\s]+$"; //used for ownername, country
     public static final String CITY_PATTERN_LETTER_AND_SPACES = "^(?!City not found$)(?!Error fetching city$)[a-zA-ZÆØÅæøåé\\s]+$";
     public static final String ADDRESS1_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé0-9\\s.,#-]+$";
     public static final String ADDRESS2_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé0-9\\s.,#-]*$";
