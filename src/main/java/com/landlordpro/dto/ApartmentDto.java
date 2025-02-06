1983c5b76012205284220383164ef978e389be74
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-06 10:24:40 +0100

    fix: refactoring

--- src/main/java/com/landlordpro/dto/ApartmentDto.java
+++ src/main/java/com/landlordpro/dto/ApartmentDto.java
@@ -3,6 +3,8 @@ package com.landlordpro.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.landlordpro.dto.constants.Patterns;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -14,20 +16,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApartmentDto {
-
-    public static final String NAME_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé\\s]+$";
-    public static final String CITY_PATTERN_LETTER_AND_SPACES = "^(?!City not found$)(?!Error fetching city$)[a-zA-ZÆØÅæøåé\\s]+$";
-    public static final String ADDRESS1_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé0-9\\s.,#-]+$";
-    public static final String ADDRESS2_PATTERN_LETTER_AND_SPACES = "^[a-zA-ZÆØÅæøåé0-9\\s.,#-]*$";
-    public static final String PINCODE_PATTERN_FOUR_DIGITS = "^[0-9]{4}$";
-    public static final String APARTMENT_NAME_PATTERN_LETTER_WITHOUT_SPACES = "^[a-zA-Z0-9]+$";
-
     private UUID id;  // UUID for apartment
 
     @NotBlank(message = "Apartment short name is mandatory")
     @Size(min = 2, max = 40, message = "Apartment short name must be between 2 and 40 characters")
     @Pattern(
-        regexp = APARTMENT_NAME_PATTERN_LETTER_WITHOUT_SPACES,
+        regexp = Patterns.APARTMENT_NAME_PATTERN_LETTER_WITHOUT_SPACES,
         message = "Name must contain only letters without spaces or special characters"
     )
     private String apartmentShortName;
@@ -35,7 +29,7 @@ public class ApartmentDto {
     @NotBlank(message = "Owner name is mandatory")
     @Size(min = 2, max = 100, message = "Owner name must be between 2 and 100 characters")
     @Pattern(
-        regexp = NAME_PATTERN_LETTER_AND_SPACES,
+        regexp = Patterns.NAME_PATTERN_LETTER_AND_SPACES,
         message = "Owner name must contain only letters and spaces"
     )
     private String ownerName;
@@ -43,14 +37,14 @@ public class ApartmentDto {
     @NotBlank(message = "AddressLine1 is mandatory")
     @Size(min = 2, max = 100, message = "AddressLine1 must be between 2 and 100 characters")
     @Pattern(
-        regexp = ADDRESS1_PATTERN_LETTER_AND_SPACES,
+        regexp = Patterns.ADDRESS1_PATTERN_LETTER_AND_SPACES,
         message = "AddressLine1 must contain only letters, spaces or special characters"
     )
     private String addressLine1;
 
     @Size(max = 100, message = "AddressLine2 must be between 0 and 100 characters")
     @Pattern(
-        regexp = ADDRESS2_PATTERN_LETTER_AND_SPACES,
+        regexp = Patterns.ADDRESS2_PATTERN_LETTER_AND_SPACES,
         message = "AddressLine2 must contain only letters, spaces or special characters"
     )
     private String addressLine2;
@@ -58,7 +52,7 @@ public class ApartmentDto {
     @NotBlank(message = "Pincode is mandatory")
     @Size(min = 4, max = 4, message = "Pincode must be of 4 digits")
     @Pattern(
-        regexp = PINCODE_PATTERN_FOUR_DIGITS,
+        regexp = Patterns.PINCODE_PATTERN_FOUR_DIGITS,
         message = "Pincode must contain only 4 digits"
     )
     private String pincode;
@@ -66,7 +60,7 @@ public class ApartmentDto {
     @NotBlank(message = "City is mandatory")
     @Size(max = 100, message = "City should be up to 100 characters")
     @Pattern(
-        regexp = CITY_PATTERN_LETTER_AND_SPACES,
+        regexp = Patterns.CITY_PATTERN_LETTER_AND_SPACES,
         message = "City must contain only letters and spaces"
     )
     private String city;
@@ -74,7 +68,7 @@ public class ApartmentDto {
     @NotBlank(message = "Country is mandatory")
     @Size(max = 100, message = "Country should be up to 100 characters")
     @Pattern(
-        regexp = NAME_PATTERN_LETTER_AND_SPACES,
+        regexp = Patterns.NAME_PATTERN_LETTER_AND_SPACES,
         message = "Country must be valid and contain only letters and spaces"
     )
     private String country;
