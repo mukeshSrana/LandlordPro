7617a1baefb027509ed0fe51c902fda55be00746
Mukesh Singh Rana <mukesh.rana@statnett.no>
2025-02-11 20:52:46 +0100

    fix: remove logging at service

---
+++ src/main/resources/static/js/tenantFilterTable.js
@@ -0,0 +1,47 @@
+function enableEditing(cell) {
+  // Make sure only expense name and amount cells are editable
+  if (cell.tagName !== "TD") return; // Ensure it's a table cell
+
+  // Check if the cell is already editable
+  if (cell.isContentEditable) return;
+
+  // Set contentEditable to true for direct inline editing
+  cell.contentEditable = true;
+  cell.focus();
+
+  // Add an event listener to update hidden inputs when editing is complete
+  cell.addEventListener('blur', () => {
+    cell.contentEditable = false; // Disable editing
+
+    // Find the hidden input corresponding to this cell
+    const row = cell.closest('tr');
+    const monthlyRentHiddenInput = row.querySelector('input[name="monthlyRent"]');
+    const securityDepositHiddenInput = row.querySelector('input[name="securityDeposit"]');
+    const securityDepositInstitutionNameHiddenInput = row.querySelector('input[name="securityDepositInstitutionName"]');
+
+    // Update the hidden input values based on the edited cell content
+    if (cell.cellIndex === 4 && monthlyRentHiddenInput) {
+      monthlyRentHiddenInput.value = cell.textContent.trim();
+    } else if (cell.cellIndex === 5 && securityDepositHiddenInput) {
+      securityDepositHiddenInput.value = cell.textContent.trim();
+    } else if (cell.cellIndex === 6 && securityDepositInstitutionNameHiddenInput) {
+      securityDepositInstitutionNameHiddenInput.value = cell.textContent.trim();
+    }
+  });
+}
+function syncDateBeforeSubmit(form) {
+  const row = form.closest("tr");
+  const visibleLeaseStartDateInput = row.querySelector('input[type="visibleLeaseStartDate"]');
+  const hiddenLeaseStartDateInput = form.querySelector('input[name="leaseStartDate"]');
+  if (visibleLeaseStartDateInput && hiddenLeaseStartDateInput) {
+    hiddenLeaseStartDateInput.value = visibleLeaseStartDateInput.value;
+  }
+
+  const visibleLeaseEndDateInput = row.querySelector('input[type="visibleLeaseEndDate"]');
+  const hiddenLeaseEndDateInput = form.querySelector('input[name="leaseEndDate"]');
+  if (visibleLeaseEndDateInput && hiddenLeaseEndDateInput) {
+    hiddenLeaseEndDateInput.value = visibleLeaseEndDateInput.value;
+  }
+}
+
+
